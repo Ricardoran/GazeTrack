be6721f205b341c0f6305bf99ff8c9f002affb4a
Ricardoran <ricardo15222024@gmail.com>
2025-03-02 13:36:54 -0800

    Initial Commit

---
+++ GazeTrackApp/GazeTrackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeTrackAppApp.swift
+//  GazeTrackApp
+//
+//  Created by Haoran Zhang on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GazeTrackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
